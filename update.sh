792a7b1aa37450adc450666fd330c4675f0a62cf
Vicky <vicky@vickylai.com>
2019-01-23 09:37:09 -0500

    Fix bug in build output filename

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-GOOS=linux go build && \
+GOOS=linux go build -o authorAFK && \
 zip authorAFK.zip authorAFK && \
 rm authorAFK && \
 aws lambda update-function-code --function-name authorAFK --zip-file fileb://authorAFK.zip && \
